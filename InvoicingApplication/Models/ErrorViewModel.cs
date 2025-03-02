a1571cad1e09f36c66ead8734f919fc36628e6f5
serpi <serpi@SERPILTEKE>
2025-03-03 01:54:26 +0300

    Add project files.

---
+++ InvoicingApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InvoicingApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
