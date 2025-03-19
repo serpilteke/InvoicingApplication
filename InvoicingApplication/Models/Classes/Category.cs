1562d1db1320269b241c36e69915295a2e5f2cd6
Serpil Teke <serpilteke16@gmail.com>
2025-03-19 16:14:20 +0300

    Update Category.cs

--- InvoicingApplication/Models/Classes/Category.cs
+++ InvoicingApplication/Models/Classes/Category.cs
@@ -8,6 +8,7 @@ namespace InvoicingApplication.Models.Classes
         [Key]//Primary Key olduğunu belirtmek için
         public int Id { get; set; }
         public string Name { get; set; } = null!;
+        public string Description {get; set;} = null!;
         public ICollection<Products> Products { get; set; }//her bir kategoride birden fazla ürün olabilir anlamına gelir.
 
     }
